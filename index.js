6e71aab51319d0c6630488fcbe724b1cfed6e93f
gregoris <gregodelacruz161@gmail.com>
2024-11-22 18:26:06 -0400

    creando las variables del proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+// Sección 1: Selección de Elementos y Definiciones Iniciales
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let LIST
+let id // para que inicie en 0 cada tarea tendrá un id diferente
