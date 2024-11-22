8abcb22e46d5bba52ca18c1f773b18927eed7960
gregoris <gregodelacruz161@gmail.com>
2024-11-22 18:44:45 -0400

    creando funcion para agregar tareas

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ let id // para que inicie en 0 cada tarea tendrá un id diferente
 // Sección 2: Creación de Fecha Actualizada
 const FECHA = new Date()
 fecha.innerHTML = FECHA.toLocaleDateString('es-MX', { weekday: 'long', month: 'short', day: 'numeric' })
+
+// Sección 3: Función para Agregar Tarea
+function agregarTarea(tarea, id, realizado, eliminado) {
+    if (eliminado) { return } // si existe eliminado es true si no es false
+    const REALIZADO = realizado ? check : uncheck // si realizado es verdadero check si no uncheck
+    const LINE = realizado ? lineThrough : ''
+    const elemento =` 
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                        `
+                    
+    lista.insertAdjacentHTML("beforeend", elemento)
+}
